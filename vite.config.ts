c045140f4b038f75e4125dd56e45910e19c26825
IamAwesomeGeorge <83273399+IamAwesomeGeorge@users.noreply.github.com>
2025-09-22 13:55:13 +0100

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
+import eslintPlugin from 'vite-plugin-eslint';
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),  eslintPlugin({
+      include: ['src/**/*.js', 'src/**/*.ts', 'src/**/*.vue', 'src/**/*.jsx', 'src/**/*.tsx'],
+      exclude: ['node_modules'],
+      cache: false,      
+      fix: true,         
+    })],
 })
